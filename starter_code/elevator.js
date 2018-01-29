bb337aaf42bea6052720bfc228e0e83c2c80a8a3
Victor Pellerin <pellerin.victor@gmail.com>
2018-01-29 21:34:31 +0100

    first commit

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,86 @@
 class Elevator {
-  constructor(){
+  constructor(direction, currentPosition){
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction = direction;
+    this.currentPosition = currentPosition;
   }
 
-  start() { }
-  stop() { }
-  update() { }
+
+  start() {
+    this.listeningInterval = setInterval(this.update(), 1000);
+   }
+
+  stop() {
+    clearInterval(listeningInterval);
+   }
+
+  update() {
+    this.log();
+   }
+
   _passengersEnter() { }
+
   _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+
+  floorUp() {
+    if (this.currentPosition < this.MAXFLOOR){
+      this.currentPosition++;
+      this.direction = 'up';
+    } 
+   }
+
+  floorDown() {
+    if (this.currentPosition > this.floor){
+      this.currentPosition--;
+      this.direction = 'down';
+    }
+   }
+
+  call(Person1) {
+    this.requests.push(Person1)
+   }
+
+  log() {
+    console.log(this.currentPosition);
+    console.log(this.direction);
+   }
 }
+
+ascensor = new Elevator ('up', 0);
+ascensor.update();
+ascensor.floorUp();
+ascensor.update();
+ascensor.floorUp();
+ascensor.update();
+ascensor.floorUp();
+ascensor.update();
+ascensor.floorUp();
+ascensor.update();
+ascensor.floorUp();
+ascensor.update();
+ascensor.floorUp();
+ascensor.update();
+ascensor.floorUp();
+ascensor.update();
+ascensor.floorUp();
+ascensor.update();
+ascensor.floorUp();
+ascensor.update();
+ascensor.floorUp();
+ascensor.update();
+ascensor.floorUp();
+ascensor.update();
+ascensor.floorUp();
+ascensor.update();
+ascensor.floorUp();
+ascensor.update();
+ascensor.floorUp();
+ascensor.update();
+ascensor.floorUp();
+ascensor.update();
+ascensor.floorUp();
+ascensor.update();
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,5 @@
 const Elevator = require('./elevator.js');
+const Elevator = require('./person.js');
+
+let person1 = new Person ('John', 0, 6);
+let person2 = new Person ('James', 5, 8);
